d0951bba65e0ebd55294fd94a5be5f5af24e5887
wally_yoon <bluegmlduf2@naver.com>
2021-07-05 22:02:58 +0900

    first commit

---
+++ moigosa_0705/test.js
@@ -0,0 +1,23 @@
+function solution(answers) {
+    var answer = [];
+    const supo1=null
+    const supo2=null
+    const supo3=null
+
+    answers.forEach((e,i)=>{
+        
+        // if(){
+
+        // }else if(){
+
+        // }else if(){
+
+        // }
+        
+    })
+
+    return answer;
+}
+
+
+solution([1,2,3,4,5])
