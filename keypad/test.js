b24642e55e10907cbb4ebc37cacf5bab2388460e
wally_yoon <bluegmlduf2@naver.com>
2021-08-04 01:23:21 +0900

    20210804 카카오인턴 키패드 완료..3일걸림

--- keypad/test.js
+++ keypad/test.js
@@ -1,7 +1,7 @@
 function solution(numbers,hand) {
     var answer = '';
-    var leftInput=""
-    var rightInput=""
+    var leftInput=10
+    var rightInput=12
     hand=="left"?hand="L":hand="R"
     
     answer=numbers.map((v,i)=>{
@@ -20,46 +20,38 @@ function solution(numbers,hand) {
         var leftDist=0
         var rightDist=0
 
+        v==0?v=11:null
+        
+        // 포인트 : * 0 # 을 10의자릿수로 변환
+        // * : 10 
+        // 0 : 11
+        // # : 12
+        // v : 2 5 8 0
+        
         //왼손의 거리구하기
-        if(v==0){
-            if (leftInput==1) {
-                leftDist=4
-            }else if (leftInput==2||leftInput==4) {
-                leftDist=3
-            }else if (leftInput==5||leftInput==7) {
-                leftDist=2
-            }else if (leftInput==8) {
-                leftDist=1
-            }else if(!leftInput){
-                leftDist=0
-            }
+        if(v==leftInput){
+            leftDist=0
         }else if (v+1==leftInput||v-1==leftInput||v+3==leftInput||v-3==leftInput) {
             leftDist=1
         }else if (v+2==leftInput||v-2==leftInput||v+4==leftInput||v-4==leftInput||v+6==leftInput||v-6==leftInput) {
             leftDist=2
-        }else if (v+5==leftInput||v-5==leftInput||v+7==leftInput||v-7==leftInput) {
+        }else if (v+5==leftInput||v-5==leftInput||v+7==leftInput||v-7==leftInput||v+9==leftInput||v-9==leftInput) {
             leftDist=3
+        }else if(v+8==leftInput||v-8==leftInput||v+10==leftInput||v-10==leftInput){
+            leftDist=4
         }
         
         //오른손의 거리구하기
-        if(v==0){
-            if (rightInput==3) {
-                rightDist=4
-            }else if (rightInput==2||rightInput==6) {
-                rightDist=3
-            }else if (rightInput==5||rightInput==9) {
-                rightDist=2
-            }else if (rightInput==8) {
-                rightDist=1
-            }else if(!rightInput){
-                rightDist=0
-            }
+        if(v==rightInput){
+            rightDist=0
         }else if (v+1==rightInput||v-1==rightInput||v+3==rightInput||v-3==rightInput) {
             rightDist=1
         }else if (v+2==rightInput||v-2==rightInput||v+4==rightInput||v-4==rightInput||v+6==rightInput||v-6==rightInput) {
             rightDist=2
-        }else if (v+5==rightInput||v-5==rightInput||v+7==rightInput||v-7==rightInput) {
+        }else if (v+5==rightInput||v-5==rightInput||v+7==rightInput||v-7==rightInput||v+9==rightInput||v-9==rightInput) {
             rightDist=3
+        }else if(v+8==rightInput||v-8==rightInput||v+10==rightInput||v-10==rightInput){
+            rightDist=4
         }
 
         //거리에 따른 손지정
@@ -75,7 +67,7 @@ function solution(numbers,hand) {
             return hand
         }
     })
-    console.log(answer.join(""))
+    //console.log(answer.join(""))
     return answer.join("");
 }
-solution([7, 0, 8, 2, 8, 3, 1, 5, 7, 6, 2],"left")
+//solution([7, 0, 8, 2, 8, 3, 1, 5, 7, 6, 2],"left")
