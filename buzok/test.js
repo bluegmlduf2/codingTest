42250cdac95720901b816d14ebe4171d1c3f887e
wally_yoon <bluegmlduf2@naver.com>
2021-08-09 01:52:43 +0900

    0809 1주차

--- buzok/test.js
+++ buzok/test.js
@@ -2,13 +2,17 @@ function solution(price, money, count) {
     var answer = 0;
     let total=0;
     
+    //중첩
     for(let i=1;i<count+1;i++){
         total=total+(price*i)   
-        console.log(11) 
     }
-    (money-total)>0?(money-total):null
-    
-    return answer;
+
+    //부족금액
+    if(total-money>0){
+        answer=(money-total)
+    }    
+
+    return Math.abs(answer);
 }
 
 solution(3, 20, 4)
